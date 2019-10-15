12c303e48e7f4367b55bd5ac00095a615286eee2
itachy <jevo747@gmail.com>
2019-10-15 20:41:21 +0800

    initialize project, with two problem resolve codes

---
+++ src/archives_1_50/mod.rs
@@ -0,0 +1,11 @@
+/*
+create at `2019-10-12` by `itachy`
+*/
+
+pub mod archives_1_10;
+
+
+pub fn entry() {
+    archives_1_10::problem_1::entry();
+    archives_1_10::problem_2::entry();
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+/*
+create at `2019-10-12` by `itachy`
+*/
+
+mod archives_1_50;
+
+pub fn run() {
+    archives_1_50::entry();
+}
